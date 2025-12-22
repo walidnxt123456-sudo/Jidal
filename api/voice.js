02b8008a979b7f5bcd71e5755126b5bfed7ba9b6
walidnxt123456-sudo <walid.nxt123456@gmail.com>
2025-12-23 00:21:54 +0100

    Delete api directory

--- api/voice.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { text } = req.body;
-
-  // Call a safe TTS provider here
-  const audioBuffer = await generateAudioBuffer(text); // pseudocode
-
-  res.setHeader("Content-Type", "audio/mpeg");
-  res.send(audioBuffer);
-}
